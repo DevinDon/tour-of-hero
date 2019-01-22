eee737311b7d1033f88d1bba2631e02f76451725
DevinDon <DevinDon@Foxmail.com>
2019-01-23 04:17:56 +0800

    feat: 6.1 添加路由

--- client/src/app/module/routing.module.ts
+++ client/src/app/module/routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroListComponent } from '../component/hero/hero-list/hero-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'hero-list', component: HeroListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
