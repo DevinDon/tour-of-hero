75f108c92bc27bf1e9e2452e6fe76153e8b7eb54
DevinDon <DevinDon@Foxmail.com>
2019-01-23 14:48:40 +0800

    feat: 6.4 添加详情视图

--- client/src/app/module/routing.module.ts
+++ client/src/app/module/routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroListComponent } from '../component/hero/hero-list/hero-list.component';
 import { DashboardComponent } from '../component/dashboard/dashboard.component';
+import { HeroDetailComponent } from '../component/hero/hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'hero-list', component: HeroListComponent },
   { path: '', pathMatch: 'full', redirectTo: '/dashboard' }
 ];
