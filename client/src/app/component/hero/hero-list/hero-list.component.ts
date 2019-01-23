bbe675821e51c56caa4603ef2c25a615585ed989
DevinDon <DevinDon@Foxmail.com>
2019-01-24 04:26:23 +0800

    feat: 7.4 删除英雄

--- client/src/app/component/hero/hero-list/hero-list.component.ts
+++ client/src/app/component/hero/hero-list/hero-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { Hero } from 'src/app/other/@types';
 import { APPService } from 'src/app/service/app.service';
 import { HeroService } from 'src/app/service/hero.service';
@@ -26,7 +27,14 @@ export class HeroListComponent implements OnInit {
     const name = this.name;
     this.heroService
       .addHero({ id: 0, name })
+      .pipe(tap(v => this.name = ''))
       .subscribe(v => this.heroes.push({ id: v, name }));
+  }
+
+  deleteHero(id: number): void {
+    this.heroService
+      .deleteHero(id)
+      .subscribe(v => this.heroes = this.heroes.filter(value => value.id !== v));
   }
 
   getHeroes(): void {
