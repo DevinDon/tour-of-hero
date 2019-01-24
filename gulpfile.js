ae60adef7c8cf4c8a4cb8d807d0d82dcc259e6c4
DevinDon <DevinDon@Foxmail.com>
2019-01-24 15:50:26 +0800

    perf: 优化打包逻辑

--- gulpfile.js
+++ gulpfile.js
@@ -39,10 +39,12 @@ gulp.task('clean', clean);
 
 // Pack app to /dist folder, x86/64 platform
 gulp.task('pack', async () => {
-  gulp.src(PATH.docker.path + '**/*')
+  gulp.src(PATH.docker.path + 'config/**/*')
+    .pipe(gulp.dest(PATH.dist + 'config'));
+  gulp.src(PATH.docker.path + 'server/Dockerfile')
+    .pipe(gulp.dest(PATH.dist + 'server'));
+  gulp.src(PATH.docker.path + 'docker-compose.yml')
     .pipe(gulp.dest(PATH.dist));
-  gulp.src([PATH.dist + 'server/Dockerfile.arm32v7', PATH.dist + 'docker-compose.arm32v7.yml'])
-    .pipe(gulpClean());
   gulp.src(PATH.client.dist + '**/*')
     .pipe(gulp.dest(PATH.dist + 'client'));
   gulp.src([PATH.server.dist + '**/*.js', PATH.server.path + '*.json'])
