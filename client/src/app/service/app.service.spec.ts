7bf41cdece0db4e22203c467d063ba87d993667c
DevinDon <DevinDon@Foxmail.com>
2019-01-26 03:47:51 +0800

    feat(service): 更新测试

--- client/src/app/service/app.service.spec.ts
+++ client/src/app/service/app.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
 import { APPService } from './app.service';
 
-describe('AppService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: APPService = TestBed.get(APPService);
+describe('APPService', () => {
+
+  let service: APPService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ providers: [APPService] });
+  });
+
+  it('APPService should be created', () => {
+    service = TestBed.get(APPService);
     expect(service).toBeTruthy();
   });
+
 });
--- client/src/app/service/hero.service.spec.ts
+++ client/src/app/service/hero.service.spec.ts
@@ -1,12 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
+import { Hero } from '../other/@types';
 
 describe('HeroService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: HeroService = TestBed.get(HeroService);
-    expect(service).toBeTruthy();
+  let service: HeroService;
+  let httpClient: jasmine.SpyObj<HttpClient>;
+  let messageService: jasmine.SpyObj<MessageService>;
+
+  beforeEach(() => {
+    const httpSpy = jasmine.createSpyObj('HttpClient', ['post', 'delete', 'get', 'put']);
+    const messageSpy = jasmine.createSpyObj('MessageService', ['message', 'add', 'clear']);
+    TestBed.configureTestingModule({
+      providers: [
+        HeroService,
+        { provide: HttpClient, useValue: httpSpy },
+        { provide: MessageService, useValue: messageSpy }
+      ]
+    });
   });
+
+  it('HeroService, HttpClient & MessageService should be created', () => {
+    service = TestBed.get(HeroService);
+    httpClient = TestBed.get(HttpClient);
+    messageService = TestBed.get(MessageService);
+    expect(service).toBeTruthy();
+    expect(httpClient).toBeTruthy();
+    expect(messageService).toBeTruthy();
+  });
+
+  it('#addHero should add a hero to server', done => {
+    const hero: Hero = { id: 0, name: 'Test Hero' };
+    // service.addHero(hero).subscribe(v => {
+    //   expect(v).toBeGreaterThan(0);
+    // });
+    // expect
+    done();
+  });
+
 });
