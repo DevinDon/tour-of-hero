8d1234e01f0dae0d70a99ffbf9a13b3ef8d77b0f
DevinDon <DevinDon@Foxmail.com>
2019-01-26 04:02:10 +0800

    feat(fill-with.pipe.test): add test example

--- client/src/app/other/pipe/fill-with.pipe.spec.ts
+++ client/src/app/other/pipe/fill-with.pipe.spec.ts
@@ -1,8 +1,40 @@
 import { FillWithPipe } from './fill-with.pipe';
 
 describe('FillWithPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FillWithPipe();
+
+  let pipe: FillWithPipe;
+
+  it('FillWithPipe create an instance', () => {
+    pipe = new FillWithPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('transform one digit to two, input number, fill with default char', done => {
+    for (let i = 0; i < 10; i++) {
+      expect(pipe.transform(i, 2)).toBe('0' + i);
+    }
+    done();
+  });
+
+  it('transform one digit to two, input string, fill with default value', done => {
+    for (let i = 0; i < 10; i++) {
+      expect(pipe.transform(i.toString(), 2)).toBe('0' + i);
+    }
+    done();
+  });
+
+  it('transform two digit to five, input number, fill with char "X"', done => {
+    for (let i = 10; i < 100; i++) {
+      expect(pipe.transform(i, 5, 'X')).toBe('XXX' + i);
+    }
+    done();
+  });
+
+  it('transform two digit to one, input number, fill with char "A"', done => {
+    for (let i = 10; i < 100; i++) {
+      expect(pipe.transform(i, 1, 'A')).toBe(i.toString());
+    }
+    done();
+  });
+
 });
