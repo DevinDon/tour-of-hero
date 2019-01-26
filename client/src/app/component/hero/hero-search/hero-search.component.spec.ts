a3cdb7b7e6ffeb10b9d221415aed65458098cdcf
DevinDon <DevinDon@Foxmail.com>
2019-01-26 20:55:37 +0800

    test(hero-search.component): add basic test

--- client/src/app/component/hero/hero-search/hero-search.component.spec.ts
+++ client/src/app/component/hero/hero-search/hero-search.component.spec.ts
@@ -1,16 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { HeroService } from 'src/app/service/hero.service';
+import { MockHeroService } from 'src/app/service/hero.service.spec';
+import { StubHeroListComponent } from '../hero-list/hero-list.component.spec';
 import { HeroSearchComponent } from './hero-search.component';
 
 describe('HeroSearchComponent', () => {
+
   let component: HeroSearchComponent;
   let fixture: ComponentFixture<HeroSearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroSearchComponent ]
+      imports: [FormsModule],
+      declarations: [
+        HeroSearchComponent,
+        StubHeroListComponent
+      ],
+      providers: [
+        { provide: HeroService, useClass: MockHeroService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +33,5 @@ describe('HeroSearchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
