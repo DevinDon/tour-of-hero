30494269900dfd561c54949fdcb27b81c9041b4b
DevinDon <DevinDon@Foxmail.com>
2019-01-26 20:40:33 +0800

    test(hero-list.component): add basic test

--- client/src/app/component/hero/hero-list/hero-list.component.spec.ts
+++ client/src/app/component/hero/hero-list/hero-list.component.spec.ts
@@ -1,16 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroListComponent } from './hero-list.component';
+import { FillWithPipe } from 'src/app/other/pipe/fill-with.pipe';
+import { APPService } from 'src/app/service/app.service';
+import { HeroService } from 'src/app/service/hero.service';
+import { MockHeroService } from 'src/app/service/hero.service.spec';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeroListComponent', () => {
+
   let component: HeroListComponent;
   let fixture: ComponentFixture<HeroListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroListComponent ]
+      imports: [RouterTestingModule],
+      declarations: [HeroListComponent, FillWithPipe],
+      providers: [
+        APPService,
+        { provide: HeroService, useClass: MockHeroService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +33,5 @@ describe('HeroListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
