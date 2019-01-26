607682ce35527d61d2a20f2434ed51078e99338c
DevinDon <DevinDon@Foxmail.com>
2019-01-26 21:46:29 +0800

    feat(hero/add): add value check

--- server/src/router/POST/hero/add.ts
+++ server/src/router/POST/hero/add.ts
@@ -1,22 +1,33 @@
 import { Middleware } from 'koa';
 import Hero from '../../../entity/hero.entity';
 import { generateResponse } from '../../function';
-
+import 'koa-body';
 type Request = Partial<Hero>;
-type Content = number;
+type Content = Partial<Hero>;
 
 export const hero$add: Middleware = async (c, next) => {
   next();
   const request: Request = {
     name: c.request.body.name
   };
-  const result = await Hero.insert(request);
-  c.body = generateResponse<Content>({
-    status: {
-      message: `Added a new hero, ID: ${result.identifiers[0] && result.identifiers[0].id}`
-    },
-    content: result.identifiers[0] && result.identifiers[0].id
-  });
+  if (request.name) {
+    const result = await Hero.insert(request);
+    c.body = generateResponse<Content>({
+      status: {
+        message: `Added a new hero, ID: ${result.identifiers[0] && result.identifiers[0].id}`
+      },
+      content: {
+        id: result.identifiers[0] && result.identifiers[0].id,
+        name: request.name
+      }
+    });
+  } else {
+    c.body = generateResponse<Content>({
+      status: {
+        message: `Invalid hero information.`
+      }
+    });
+  }
 };
 
 export default hero$add;
