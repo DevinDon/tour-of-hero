12a79bdd6bc07327873581b5a828a286d7473f3f
DevinDon <DevinDon@Foxmail.com>
2019-01-26 20:29:44 +0800

    test(hero-detail.component): add test

--- client/src/app/component/hero/hero-detail/hero-detail.component.spec.ts
+++ client/src/app/component/hero/hero-detail/hero-detail.component.spec.ts
@@ -1,16 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from 'src/app/service/hero.service';
+import { of, Observable } from 'rxjs';
+import { Hero } from 'src/app/other/@types';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Location } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FillWithPipe } from 'src/app/other/pipe/fill-with.pipe';
+
+const mockHeroes: Hero[] = [
+  { id: 1, name: 'Test1' },
+  { id: 2, name: 'Test2' }
+];
+
+class MockHeroService {
+
+  getHero(id: number): Observable<Hero> {
+    return of(mockHeroes.find(v => v.id === id));
+  }
+
+  updateHero(hero: Hero) {
+    return of(hero.id);
+  }
+
+}
 
 describe('HeroDetailComponent', () => {
+
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroDetailComponent ]
+      imports: [RouterTestingModule, FormsModule],
+      declarations: [HeroDetailComponent, FillWithPipe],
+      providers: [
+        { provide: HeroService, useClass: MockHeroService },
+        // { provide: Location, useValue: jasmine.createSpyObj('Location', ['back']) }
+        Location
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +50,8 @@ describe('HeroDetailComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('HeroDetail should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
