7c61c858e6ef7fe382e2423cb413bcc9424d9d82
IInfinity <I.INF@Outlook.com>
2019-06-23 01:56:37 +0800

    feat(component/hero): add hero top component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeroDetailComponent } from './hero/hero-detail/hero-detail.component';
 import { HeroListComponent } from './hero/hero-list/hero-list.component';
 import { MaterialModule } from './module/material.module';
 import { RoutingModule } from './module/routing.module';
+import { HeroTopComponent } from './hero/hero-top/hero-top.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroListComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroTopComponent
   ],
   imports: [
     BrowserModule,
