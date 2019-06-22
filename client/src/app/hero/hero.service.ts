5d43b83cd2db7af99832ed16fb273f78e410ecab
IInfinity <I.INF@Outlook.com>
2019-06-23 03:09:16 +0800

    feat(service/hero): add hero service

--- client/src/app/hero/hero.service.ts
+++ client/src/app/hero/hero.service.ts
@@ -1,10 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { filter, mergeAll, map } from 'rxjs/operators';
+import { Hero } from './hero.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  static readonly API$HEROES = 'assets/heroes.json';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getOne(id: number): Observable<Hero> {
+    return this.http
+      .get<Hero[]>(HeroService.API$HEROES)
+      .pipe(
+        mergeAll(),
+        filter(hero => hero.id === id)
+      );
+  }
+
+  getTop(total: number = 4): Observable<Hero[]> {
+    return this.http
+      .get<Hero[]>(HeroService.API$HEROES)
+      .pipe(map(heroes => heroes.slice(0, total)));
+  }
+
+  getAll(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(HeroService.API$HEROES);
+  }
 
 }
