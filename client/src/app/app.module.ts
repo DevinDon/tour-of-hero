10d7fffab2f644874e2938316e87915b30074fa3
IInfinity <I.INF@Outlook.com>
2019-06-23 01:46:43 +0800

    refactor(component): rename detail to hero-detail

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { DetailComponent } from './detail/detail.component';
-import { ListComponent } from './list/list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
 import { MaterialModule } from './module/material.module';
 import { RoutingModule } from './module/routing.module';
 
@@ -12,8 +12,8 @@ import { RoutingModule } from './module/routing.module';
   declarations: [
     AppComponent,
     DashboardComponent,
-    ListComponent,
-    DetailComponent
+    HeroListComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/hero-detail/hero-detail.component.spec.ts
+++ client/src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DetailComponent } from './detail.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
-describe('DetailComponent', () => {
-  let component: DetailComponent;
-  let fixture: ComponentFixture<DetailComponent>;
+describe('HeroDetailComponent', () => {
+  let component: HeroDetailComponent;
+  let fixture: ComponentFixture<HeroDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DetailComponent ]
+      declarations: [ HeroDetailComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DetailComponent);
+    fixture = TestBed.createComponent(HeroDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
