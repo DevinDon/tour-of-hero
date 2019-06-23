bf9d58f7a2e0e505b348ed5d3f6b0123396b7d69
IInfinity <I.INF@Outlook.com>
2019-06-23 13:34:55 +0800

    chore(angular): add snack bar module

--- client/src/app/module/material.module.ts
+++ client/src/app/module/material.module.ts
@@ -5,7 +5,8 @@ import {
   MatFormFieldModule,
   MatIconModule,
   MatInputModule,
-  MatProgressBarModule
+  MatProgressBarModule,
+  MatSnackBarModule
 } from '@angular/material';
 
 @NgModule({
@@ -15,7 +16,8 @@ import {
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatSnackBarModule
   ]
 })
 export class MaterialModule { }
