bbd8957d98e720ca32454c22262bf71387f47b7e
IInfinity <I.INF@Outlook.com>
2019-06-23 15:06:36 +0800

    perf(service/hero): perf method with status

--- client/src/app/hero/hero.service.ts
+++ client/src/app/hero/hero.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { filter, mergeAll, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { delay, filter, finalize, map, mergeAll } from 'rxjs/operators';
+import { AppService } from '../app.service';
+import { BaseResponse } from '../model/base.model';
 import { Hero } from './hero.type';
 
 @Injectable({
@@ -12,26 +14,46 @@ export class HeroService {
   static readonly API$HEROES = 'assets/heroes.json';
 
   constructor(
+    private app: AppService,
     private http: HttpClient
   ) { }
 
+  getAll(): Observable<Hero[]> {
+    this.app.busy();
+    return this.http
+      .get<Hero[]>(HeroService.API$HEROES)
+      .pipe(finalize(() => this.app.free()));
+  }
+
+  getTop(total: number = 4): Observable<Hero[]> {
+    this.app.busy();
+    return this.http
+      .get<Hero[]>(HeroService.API$HEROES)
+      .pipe(
+        delay(1500),
+        map(heroes => heroes.slice(0, total)),
+        finalize(() => this.app.free())
+      );
+  }
+
   getOne(id: number): Observable<Hero> {
+    this.app.busy();
     return this.http
       .get<Hero[]>(HeroService.API$HEROES)
       .pipe(
         mergeAll(),
-        filter(hero => hero.id === id)
+        filter(hero => hero.id === id),
+        finalize(() => this.app.free())
       );
   }
 
-  getTop(total: number = 4): Observable<Hero[]> {
-    return this.http
-      .get<Hero[]>(HeroService.API$HEROES)
-      .pipe(map(heroes => heroes.slice(0, total)));
-  }
-
-  getAll(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(HeroService.API$HEROES);
+  saveOne(hero: Hero): Observable<BaseResponse> {
+    this.app.busy();
+    return of<BaseResponse>({ status: true })
+      .pipe(
+        delay(1000),
+        finalize(() => this.app.free())
+      );
   }
 
 }
