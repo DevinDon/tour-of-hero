46a13f35ed4144cc359fc859057885de2a95b2fb
IInfinity <I.INF@Outlook.com>
2019-06-23 15:06:05 +0800

    perf(service/app): add status & snack bar

--- client/src/app/app.service.ts
+++ client/src/app/app.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
+import { Subject } from 'rxjs';
+import { auditTime } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +12,35 @@ export class AppService {
     loading: 0
   };
 
-  constructor() { }
+  private subjections = {
+    loading: {
+      value: 0,
+      subject: new Subject<number>()
+    }
+  };
+
+  constructor(
+    private bar: MatSnackBar
+  ) {
+    this.subjections.loading.subject
+      .pipe(auditTime(16))
+      .subscribe(v => this.status.loading = v);
+  }
 
   busy() {
-    return ++this.status.loading;
+    this.subjections.loading.subject.next(++this.subjections.loading.value);
   }
 
   free() {
-    return --this.status.loading;
+    this.subjections.loading.subject.next(--this.subjections.loading.value);
+  }
+
+  openBar(message: string, action: string = 'OK', config: MatSnackBarConfig = { duration: 3000 }) {
+    this.bar.open(message, action, config);
+  }
+
+  closeBar() {
+    this.bar.dismiss();
   }
 
 }
