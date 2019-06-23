ce45dc6e2f9d15dca7b1c601b40b4aa3f5fff956
IInfinity <I.INF@Outlook.com>
2019-06-23 12:35:06 +0800

    chore(angular): add reactive froms module

--- client/src/app/module/other.module.ts
+++ client/src/app/module/other.module.ts
@@ -1,9 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   exports: [
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ]
 })
 export class OtherModule { }
