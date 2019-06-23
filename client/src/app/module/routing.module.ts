f4014973e3545799624d6148f0ee2cf4071164ff
IInfinity <I.INF@Outlook.com>
2019-06-23 23:39:19 +0800

    feat(module/routing): add about to routing

--- client/src/app/module/routing.module.ts
+++ client/src/app/module/routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from '../about/about.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { HeroDetailComponent } from '../hero/hero-detail/hero-detail.component';
 import { HeroListComponent } from '../hero/hero-list/hero-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'hero/list', component: HeroListComponent },
   { path: 'hero/:id', component: HeroDetailComponent }
