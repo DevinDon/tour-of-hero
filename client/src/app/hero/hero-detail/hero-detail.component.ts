c566c1c2c0582d6331f20d66c1cbbd6f40919a60
IInfinity <I.INF@Outlook.com>
2019-06-23 15:13:33 +0800

    feat(component/detail): update detail

--- client/src/app/hero/hero-detail/hero-detail.component.ts
+++ client/src/app/hero/hero-detail/hero-detail.component.ts
@@ -1,4 +1,10 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { AppService } from 'src/app/app.service';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero.type';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroDetailComponent implements OnInit {
 
-  constructor() { }
+  hero: Hero;
+  formGroup: FormGroup;
+
+  constructor(
+    protected app: AppService,
+    private location: Location,
+    private route: ActivatedRoute,
+    private service: HeroService
+  ) {
+    this.formGroup = new FormGroup({
+      id: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required])
+    });
+    this.formGroup.get('id').disable();
+  }
 
   ngOnInit() {
+    this.load();
+  }
+
+  back() {
+    this.location.back();
+  }
+
+  load() {
+    this.service
+      .getOne(+this.route.snapshot.paramMap.get('id'))
+      .subscribe(hero => {
+        this.hero = hero;
+        this.formGroup.get('id').setValue(hero.id);
+        this.formGroup.get('name').setValue(hero.name);
+      });
+  }
+
+  save() {
+    const hero: Hero = {
+      id: this.formGroup.get('id').value,
+      name: this.formGroup.get('name').value
+    };
+    this.service
+      .saveOne(hero)
+      .subscribe(v => {
+        if (v.status) {
+          this.app.openBar(`Hero ${hero.name} saved successfully.`);
+        } else {
+          this.app.openBar(`Hero ${hero.name} saved failed.`);
+        }
+      });
   }
 
 }
