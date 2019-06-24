a39078a9aa4a9f2c48ed902bf6703b9dbe93915b
IInfinity <I.INF@Outlook.com>
2019-06-25 01:04:09 +0800

    feat(component/about): add about page

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface TimeEvent {
+  time: Date | number;
+  title: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,8 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  protected events: TimeEvent[] = [];
+
+  constructor() {
+    this.events.push({
+      time: new Date('2016-12-04'),
+      title: 'Hello GitHub',
+      content: 'Nice to meet you, GitHub!'
+    });
+    this.events.push({
+      time: new Date('2018-09-01'),
+      title: 'Clean GitHub',
+      content: 'I deleted all repositories on my GitHub. Maybe I\'m mad.'
+    });
+    this.events.push({
+      time: new Date('2018-10-06'),
+      title: 'First Repository',
+      content: 'Oh, there is my first repository calendar, about Angular.'
+    });
+    this.events.push({
+      time: new Date('2019-01-04'),
+      title: 'Most Contributions',
+      content: 'Oh God! I created 124 commits in 1 repository!'
+    });
+    this.events.push({
+      time: new Date('2019-06-02'),
+      title: 'Rester Framework',
+      content: 'I started a new tour of coding, named Rester. A TypeScript framework, like Spring Boot.'
+    });
+    this.events.push({
+      time: new Date('2019-06-24'),
+      title: 'What\'s Now',
+      content: 'I had created 2,253 contributions in the last year, from 2018-10-06 to 2019-06-24'
+    });
+  }
 
   ngOnInit() { }
+
+  trackByFn(index: number, item: TimeEvent) {
+    return item.time;
+  }
 
 }
