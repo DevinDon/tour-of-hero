c94aaf58504d6f629e389d727f8e9d28f13a89b7
IInfinity <I.INF@Outlook.com>
2019-06-25 01:06:07 +0800

    perf(module/routing): remove detail & add top

--- client/src/app/module/routing.module.ts
+++ client/src/app/module/routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from '../about/about.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
-import { HeroDetailComponent } from '../hero/hero-detail/hero-detail.component';
 import { HeroListComponent } from '../hero/hero-list/hero-list.component';
+import { HeroTopComponent } from '../hero/hero-top/hero-top.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'hero/list', component: HeroListComponent }
+  { path: 'hero/list', component: HeroListComponent },
+  { path: 'hero/top', component: HeroTopComponent }
 ];
 
 @NgModule({
