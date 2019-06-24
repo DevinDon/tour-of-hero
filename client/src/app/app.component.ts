a50b353ee3816466fc0227a41cb6b8a1674f4f84
IInfinity <I.INF@Outlook.com>
2019-06-25 03:09:25 +0800

    perf(component/app): perf app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,64 @@
-import { Platform } from '@angular/cdk/platform';
-import { Location } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { AppService } from './app.service';
+
+interface Route {
+  path: string;
+  icon: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  title = 'Tour of Heroes';
+  protected routes: Route[] = [{
+    path: '/dashboard',
+    icon: 'dashboard',
+    title: 'Dashboard'
+  },
+  {
+    path: '/hero/list',
+    icon: 'format_list_numbered',
+    title: 'Hero List'
+  },
+  {
+    path: '/hero/tops',
+    icon: 'whatshot',
+    title: 'Tops Hero'
+  },
+  {
+    path: '/about',
+    icon: 'info_outline',
+    title: 'About'
+  }];
+
+  protected title: Route = {
+    path: '/',
+    icon: '',
+    title: 'Tour of Heroes'
+  };
 
   constructor(
     protected app: AppService,
-    private location: Location,
-    protected platform: Platform
+    private router: Router
   ) { }
 
-  isPath(path: string) {
-    return this.location.path() === path;
+  ngOnInit() {
+    this.router.events
+      .subscribe(v => {
+        if (v instanceof NavigationEnd) {
+          this.title = this.routes.find(route => route.path === v.urlAfterRedirects);
+
+        }
+      });
+  }
+
+  trackByFn(index: number, item: Route) {
+    return item.path;
   }
 
 }
