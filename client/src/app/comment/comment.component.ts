881695b52605070248f6f0bb6218101c07b4d5b3
IInfinity <I.INF@Outlook.com>
2019-06-29 01:38:55 +0800

    feat(client/comment): add comment form

--- client/src/app/comment/comment.component.ts
+++ client/src/app/comment/comment.component.ts
@@ -4,6 +4,7 @@ import { AppService } from '../app.service';
 import { Hero } from '../hero/hero.model';
 import { Comment } from './comment.model';
 import { CommentService } from './comment.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-comment',
@@ -13,16 +14,38 @@ import { CommentService } from './comment.service';
 export class CommentComponent implements OnInit {
 
   public comments: Comment[] = [];
+  public form: FormGroup;
   public hasMore = true;
 
   constructor(
-    private app: AppService,
+    public app: AppService,
     @Inject(MAT_DIALOG_DATA) public hero: Hero,
     private service: CommentService
-  ) { }
+  ) {
+    this.form = new FormGroup({
+      comment: new FormControl('', [Validators.maxLength(250)])
+    });
+  }
 
   ngOnInit() {
     this.getMoreComments();
+  }
+
+  add() {
+    this.service
+      .add({
+        belong: this.hero.id,
+        content: this.form.get('comment').value
+      })
+      .subscribe(result => {
+        if (result.status) {
+          this.comments.push(result.content);
+          this.form.get('comment').setValue('');
+          this.app.openBar(`Comment successfully.`);
+        } else {
+          this.app.openBar(`Cannot comment to this content.`);
+        }
+      });
   }
 
   getMoreComments() {
