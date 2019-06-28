e39014bb5e5234224ed1d8d190715d3b2db87c4a
IInfinity <I.INF@Outlook.com>
2019-06-28 14:37:29 +0800

    chore(server): perf rester config

--- server/src/main/index.ts
+++ server/src/main/index.ts
@@ -1,22 +1,9 @@
 import { CORSHandler, Rester } from '@rester/core';
 import { HeroController } from './hero/hero.controller';
-import { HeroEntity } from './hero/hero.entity';
 
 const rester = new Rester()
-  .configAddress
-  .setHost('0.0.0.0')
-  .setPort(8080)
-  .setProxy(true)
-  .end()
   .configControllers
   .add(HeroController)
-  .end()
-  .configDatabase
-  .setType('sqlite')
-  .setDatabase('sqlite.db')
-  .setEntities([HeroEntity])
-  .setLogging(true)
-  .setSynchronize(true)
   .end()
   .configHandlers
   .add(CORSHandler)
