b239afff59ec31d21bb17bdc1f277280a996edfb
IInfinity <I.INF@Outlook.com>
2019-06-29 00:22:18 +0800

    feat(service/comment): add comment service

---
+++ client/src/app/comment/comment.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AppService } from '../app.service';
+import { HttpClient } from '@angular/common/http';
+import { Comment } from './comment.model';
+import { BaseResponse } from '../model/base.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(
+    private app: AppService,
+    private http: HttpClient
+  ) { }
+
+  add(comment: Pick<Comment, 'belong' | 'content' | 'reply'>) {
+    return this.http
+      .post<BaseResponse<Comment>>(AppService.API$HOST + '/comment/add', comment);
+  }
+
+  getAboutHero(belong: number) {
+    return this.http
+      .get<BaseResponse<Comment[]>>(AppService.API$HOST + `/comment/hero/${belong}`);
+  }
+
+}
