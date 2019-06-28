1efa9b92bb1ff9a3805e233e4f71424350067e69
IInfinity <I.INF@Outlook.com>
2019-06-29 00:27:25 +0800

    perf(server/index): update config

--- server/src/main/index.ts
+++ server/src/main/index.ts
@@ -1,9 +1,19 @@
 import { CORSHandler, Rester } from '@rester/core';
+import { CommentController } from './comment/comment.controller';
+import { CommentEntity } from './comment/comment.entity';
 import { HeroController } from './hero/hero.controller';
+import { HeroEntity } from './hero/hero.entity';
 
 const rester = new Rester()
   .configControllers
-  .add(HeroController)
+  .add(CommentController, HeroController)
+  .end()
+  .configDatabase
+  .setType('sqlite')
+  .setDatabase('sqlite.db')
+  .setEntities([CommentEntity, HeroEntity])
+  .setLogging(true)
+  .setSynchronize(true)
   .end()
   .configHandlers
   .add(CORSHandler)
