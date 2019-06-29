c91c3f91a1f0226d3518f84079d8fcf9725d2c7c
IInfinity <I.INF@Outlook.com>
2019-06-29 08:49:59 +0800

    feat(client/routing): support route reuse

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouteReuseStrategy } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
@@ -10,7 +11,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroModule } from './hero/hero.module';
 import { MaterialModule } from './module/material.module';
 import { OtherModule } from './module/other.module';
-import { RoutingModule } from './module/routing.module';
+import { RouteReuseHandler, RoutingModule } from './module/routing.module';
 import { LoadingInterceptor } from './other/loading.interceptor';
 
 @NgModule({
@@ -32,6 +33,7 @@ import { LoadingInterceptor } from './other/loading.interceptor';
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+    { provide: RouteReuseStrategy, useClass: RouteReuseHandler },
     AppService
   ]
 })
--- client/src/app/module/routing.module.ts
+++ client/src/app/module/routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy, RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from '../about/about.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { HeroListComponent } from '../hero/hero-list/hero-list.component';
@@ -20,3 +20,29 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class RoutingModule { }
+
+export class RouteReuseHandler implements RouteReuseStrategy {
+
+  private cache: Map<string, DetachedRouteHandle> = new Map();
+
+  shouldDetach(route: ActivatedRouteSnapshot): boolean {
+    return !route.data.reload;
+  }
+
+  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
+    this.cache.set(route.routeConfig.path, handle);
+  }
+
+  shouldAttach(route: ActivatedRouteSnapshot): boolean {
+    return this.cache.has(route.routeConfig.path);
+  }
+
+  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
+    return this.cache.get(route.routeConfig.path);
+  }
+
+  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+    return future.routeConfig === curr.routeConfig;
+  }
+
+}
