047d3f10f9d6958e379657060ab422127148fbd0
IInfinity <I.INF@Outlook.com>
2019-06-30 21:15:38 +0800

    perf(client/routing): optimize routing module

--- client/src/app/module/routing.module.ts
+++ client/src/app/module/routing.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
-import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy, RouterModule, Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy, RouterModule } from '@angular/router';
 import { AboutComponent } from '../about/about.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { HeroListComponent } from '../hero/hero-list/hero-list.component';
 import { HeroTopsComponent } from '../hero/hero-tops/hero-tops.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'about', pathMatch: 'full' },
-  { path: 'about', component: AboutComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'hero/list', component: HeroListComponent },
-  { path: 'hero/tops', component: HeroTopsComponent }
-];
-
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'about', pathMatch: 'full' },
+      { path: 'about', component: AboutComponent },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'hero/list', component: HeroListComponent },
+      { path: 'hero/tops', component: HeroTopsComponent }
+    ])
   ],
   exports: [RouterModule]
 })
